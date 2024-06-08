744430163c80c4461956e046c3aef0840a0380aa
Adéla Přibylová <adelpribyl@gmail.com>
2024-06-08 15:13:20 +0200

    Class names to details

--- src/components/PositonDetail/index.jsx
+++ src/components/PositonDetail/index.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 export const PositionDetail = ({ title, description, imgSrc, imgAlt }) => {
   return (
     <div className="position-detail">
-      <h2>{title}</h2>
-      <img src={imgSrc} alt={imgAlt} />
-      <p>{description}</p>
+      <h2 className="position-detail__label">{title}</h2>
+      <img
+        className="position-detail__image-container"
+        src={imgSrc}
+        alt={imgAlt}
+      />
+      <p className="postition-detail__text">{description}</p>
     </div>
   );
 };
--- src/components/TechniqueDetail/index.jsx
+++ src/components/TechniqueDetail/index.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 export const TechniqueDetail = ({ title, description, images }) => {
   return (
     <div className="technique-detail">
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2 className="technique-detail__label">{title}</h2>
+      <p className="technique-detail__text">{description}</p>
       {images &&
         images.map((image, index) => (
           <img
             key={index}
             src={image}
             alt={`${title} image ${index + 1}`}
-            className="technique-image"
+            className="technique-detail__image"
           />
         ))}
     </div>
