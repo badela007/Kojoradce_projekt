4ae8988ad456609a2259787600b0a37eb1de0549
Adéla Přibylová <adelpribyl@gmail.com>
2024-06-08 10:28:01 +0200

    KojoradcePage, LPPage

---
+++ src/pages/KojoradcePage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+export const KojoradcePage = () => {
+  return (
+    <div className="container">
+      <header>
+        <div />
+        <h1>Kojorádce</h1>
+      </header>
+      <main>
+        <nav>Tady bude chat Kojorádce</nav>
+      </main>
+      <footer>
+        <p>Tady bude footer</p>
+      </footer>
+    </div>
+  );
+};
---
+++ src/pages/LPPage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+export const LPPage = () => {
+  return (
+    <div className="container">
+      <header>
+        <div />
+        <h1>Laktační poradkyně</h1>
+      </header>
+      <main>
+        <nav>Tady bude odkaz na laktační poradkyně</nav>
+      </main>
+      <footer>
+        <p>Tady bude footer</p>
+      </footer>
+    </div>
+  );
+};
