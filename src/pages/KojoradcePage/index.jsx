4dc4d3a99c11e3aae914e1ec7e9eaab314303c7d
Adéla Přibylová <adelpribyl@gmail.com>
2024-06-08 15:56:57 +0200

    Map

--- src/pages/KojoradcePage/index.jsx
+++ src/pages/KojoradcePage/index.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import './style.css';
 import { Question } from '../../components/Question';
 import { AnswerButton } from '../../components/AnswerButton';
-import data  from '../../json/decision_tree_2024-06-04.json';
+import data from '../../json/decision_tree_2024-06-04.json';
 import { useParams } from 'react-router-dom';
 
 export const KojoradcePage = () => {
-
   const { code } = useParams();
   console.log(code);
-  
+
   const question = data.find((qstn) => qstn.code === Number(code));
-  console.log(question)
+  console.log(question);
+
+  const answers = question.option_codes.map((code) => {
+    const answer = data.find((answr) => answr.code === code);
+  });
+  console.log(answers);
+
+  /*const answer = data.find((answr) => answr.code === question.option_codes[1]);
+  console.log(answer.text);*/
 
   return (
     <div className="container">
@@ -20,14 +27,8 @@ export const KojoradcePage = () => {
         <h1>Kojorádce</h1>
       </header>
       <main>
-      <Question text= {question.text}/>
-      <h3>{question.option_codes}</h3>
-      
-       {/* 
-      <AnswerButton text="1. odpoved z JSON" />
-      <AnswerButton text="2. odpoved z JSON" />
-      <AnswerButton text="3. odpoved z JSON" /> */}
-       
+        <Question text={question.text} />
+        <h3>{question.option_codes}</h3>
       </main>
       <footer>
         <p>Tady bude footer</p>
