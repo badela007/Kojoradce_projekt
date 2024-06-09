3262f4f8a2627c2130e0062b81e97d556aad0eeb
Anna Baranek <annaxbaranek@gmail.com>
2024-06-09 15:13:31 +0200

    feedbackpage update

--- src/components/ButtonsFinal/index.jsx
+++ src/components/ButtonsFinal/index.jsx
@@ -6,7 +6,7 @@ export const ButtonsFinal = ({link, backgroundColor, icon}) => {
     return (
 <>
     <Link to="/info-o-kojeni"><div className="answerFinal">Chci znát techniku kojení a polohy</div></Link> 
-    <Link to="/"><div className="answerFinal">Nic dalšího nepotřebuji</div></Link> 
+    <Link to="/feedback"><div className="answerFinal">Nic dalšího nepotřebuji</div></Link> 
     <Link to="/"><div className="answerFinal">Zkusit znovu</div></Link> 
 </>
     );
