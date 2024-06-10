cab2ea3ca8293cea77ff3be5fa078ba4060d398b
Anna Baranek <annaxbaranek@gmail.com>
2024-06-10 13:07:56 +0200

    bottomMenu buttons overflow solved

--- src/components/ButtonsFinal/index.jsx
+++ src/components/ButtonsFinal/index.jsx
@@ -1,16 +1,18 @@
-import './style.css';
-import { Link } from 'react-router-dom';
+import "./style.css";
+import { Link } from "react-router-dom";
 
-
-export const ButtonsFinal = ({link, backgroundColor, icon}) => {
-    return (
-<>
-    <Link to="/info-o-kojeni"><div className="answerFinal">Chci znát techniku kojení a polohy</div></Link> 
-    <Link to="/feedback"><div className="answerFinal">Nic dalšího nepotřebuji</div></Link> 
-    <Link to="/"><div className="answerFinal">Zkusit znovu</div></Link> 
-</>
-    );
-}
-
-
-
+export const ButtonsFinal = ({ link, backgroundColor, icon }) => {
+  return (
+    <div className="buttonsFinal">
+      <Link to="/info-o-kojeni">
+        <div className="answerFinal">Chci znát techniku kojení a polohy</div>
+      </Link>
+      <Link to="/feedback">
+        <div className="answerFinal">Nic dalšího nepotřebuji</div>
+      </Link>
+      <Link to="/">
+        <div className="answerFinal">Zkusit znovu</div>
+      </Link>
+    </div>
+  );
+};
