6522e1756a5393cddeaad08ce167f43a46ff75cb
Anna Baranek <annaxbaranek@gmail.com>
2024-06-11 01:49:49 +0200

    loading animation folder created

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   NavLink,
@@ -7,16 +7,16 @@ import {
   Route,
   Link,
   Outlet,
-} from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import './global.css';
-import { KojoradcePage } from './pages/KojoradcePage';
-import { InfoPage } from './pages/InfoPage';
-import { PositionsPage } from './pages/PositionsPage';
-import { TechniquePage } from './pages/TechniquePage';
-import { LPPage } from './pages/LPPage';
-import { BottomMenu } from './components/BottomMenu';
-import { FeedbackPage } from './pages/FeedbackPage';
+} from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import "./global.css";
+import { KojoradcePage } from "./pages/KojoradcePage";
+import { InfoPage } from "./pages/InfoPage";
+import { PositionsPage } from "./pages/PositionsPage";
+import { TechniquePage } from "./pages/TechniquePage";
+import { LPPage } from "./pages/LPPage";
+import { BottomMenu } from "./components/BottomMenu";
+import { FeedbackPage } from "./pages/FeedbackPage";
 
 export const ErrorPage = () => {
   return (
@@ -40,44 +40,43 @@ const App = () => {
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <HomePage />,
       },
       {
-        path: '/kojoradce/:code',
+        path: "/kojoradce/:code",
         element: <KojoradcePage />,
       },
       {
-        path: '/info-o-kojeni',
+        path: "/info-o-kojeni",
         element: <InfoPage />,
       },
       {
-        path: '/info-o-kojeni/polohy',
+        path: "/info-o-kojeni/polohy",
         element: <PositionsPage />,
       },
       {
-        path: '/info-o-kojeni/technika',
+        path: "/info-o-kojeni/technika",
         element: <TechniquePage />,
       },
 
       {
-        path: '/laktacni-poradkyne',
+        path: "/laktacni-poradkyne",
         element: <LPPage />,
       },
       {
-        path: '/feedback',
-        element: <FeedbackPage />
-
+        path: "/feedback",
+        element: <FeedbackPage />,
       },
     ],
   },
 ]);
 
-createRoot(document.querySelector('#app')).render(
-  <RouterProvider router={router} />,
+createRoot(document.querySelector("#app")).render(
+  <RouterProvider router={router} />
 );
