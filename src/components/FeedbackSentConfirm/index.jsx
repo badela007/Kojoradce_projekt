dfa041fd4b2bcdfdfd48adf53247eb72362fa4fc
Anna Baranek <annaxbaranek@gmail.com>
2024-06-12 12:06:12 +0200

    FeedBackSentConfirm component styles added

--- src/components/FeedbackSentConfirm/index.jsx
+++ src/components/FeedbackSentConfirm/index.jsx
@@ -2,18 +2,27 @@ import { Link } from "react-router-dom";
 import "./style.css";
 export const FeedbackSentConfirm = () => {
   return (
-    <div className="feedbackSent_container">
-      <div className="feedbackSent">
+    <>
+      <div className="feedbackSent_container">
         <div className="feedbackSent-title">
           <p>Odesláno.</p>
           <p>Přeji krásný den a kojení zdar!</p>
         </div>
-        <Link className="feedbackSent-toMain" to="/">
-          <button className="feedbackSent-btn" type="button">
-            Na hlavni
-          </button>
-        </Link>
       </div>
-    </div>
+      <div className="images_container">
+        <div className="image_one">
+          <img src="img/boobs_1.svg"></img>
+        </div>
+        <div className="image_two">
+          <img src="img/boobs_2.svg"></img>
+        </div>
+        <div className="image_three">
+          <img src="img/boobs_3.svg"></img>
+        </div>
+      </div>
+      <Link className="feedbackSent-toMain" to="/" type="button">
+        Na hlavni
+      </Link>
+    </>
   );
 };
