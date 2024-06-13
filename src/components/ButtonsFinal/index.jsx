4d961fc788561247babd6c928edc82b3a46e51ac
Anna Baranek <annaxbaranek@gmail.com>
2024-06-13 11:33:08 +0200

    btn to InfoPage target blank added

--- src/components/ButtonsFinal/index.jsx
+++ src/components/ButtonsFinal/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const ButtonsFinal = ({ link, backgroundColor, icon }) => {
   return (
     <div className="buttonsFinal">
-      <Link to="/info-o-kojeni">
+      <Link to="/info-o-kojeni" target="_blank">
         <div className="answerFinal">Chci znát techniku kojení a polohy</div>
       </Link>
       <Link to="/feedback">
