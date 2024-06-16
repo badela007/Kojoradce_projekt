7947c6b406974565d50584f0fa4610a48e6f9abd
Anna Baranek <annaxbaranek@gmail.com>
2024-06-16 13:53:41 +0200

    AnswerButton animation removed

--- src/components/AnswerButton/index.jsx
+++ src/components/AnswerButton/index.jsx
@@ -1,13 +1,8 @@
 import "./style.css";
 import data from "../../json/decision_tree_2024-06-04.json";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 
 export const AnswerButton = ({ code }) => {
-  const variant = {
-    hidden: { opacity: 0, y: 60 },
-    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
-  };
   const answer = data.find((answr) => answr.code === code);
 
   const navigate = useNavigate();
@@ -17,14 +12,8 @@ export const AnswerButton = ({ code }) => {
   };
 
   return (
-    <motion.div
-      variants={variant}
-      initial="hidden"
-      animate="visible"
-      onClick={handleClick}
-      className="answerButton"
-    >
+    <div onClick={handleClick} className="answerButton">
       {answer.text}
-    </motion.div>
+    </div>
   );
 };
